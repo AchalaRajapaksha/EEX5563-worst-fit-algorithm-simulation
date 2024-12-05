0f43a66f23b2b562061dbaaafee0375e26b8d962
Achala Rajpaksha <achalarajapaksha01@gmail.com>
2024-12-05 10:26:30 +0530

    input fields added

---
+++ src/Components/InputForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function InputForm() {
+  return (
+    <div>
+
+     <input type='text' placeholder='Process description'/>
+
+    </div>
+  );
+};
+
+
+export default InputForm;
